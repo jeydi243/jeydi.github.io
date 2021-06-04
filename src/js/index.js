ce63b22a25826e4063f99f6aa428954863bacd8a
jeydi243 <kadiongoe@yahoo.fr>
2021-06-04 12:04:05 +0200

    added CNAME

--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,84 @@
 import { gsap } from 'gsap'
 import '../css/style.css'
-import '../img/epa/1.jpg'
-import '../img/epa/2.jpg'
-import '../img/epa/3.jpg'
-import '../img/epa/4.jpg'
-import '../img/epa/5.jpg'
-import '../img/epa/6.jpg'
+import 'splitting/dist/splitting.css'
+import 'splitting/dist/splitting-cells.css'
+import Splitting from 'splitting'
 import LocomotiveScroll from 'locomotive-scroll'
-import fr from '../img/logoepa-v2.jpg'
-import { preloadImages, preloadFonts } from './utils'
-import { preloader } from './preloader'
-import Cursor from './cursor'
 import { ScrollTrigger } from 'gsap/all'
-// gsap.registerPlugin(ScrollTrigger)
-// var idddd = document.querySelector('[data-scroll-id]')
-// var home = document.querySelector('#home')
-// home.textContent = idddd
-// home.style.backgroundUrl = `url(${fr})`;
-const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true,
-        multiplier: 1,
-    })
-    // scroll.on('scroll', ScrollTrigger.update)
-    // let animation = gsap.timeline('.fancy', {
-    //     x: 100,
-    //     y: 200,
-    // })
-    // console.log({ idddd })
-    // ScrollTrigger.create({
-    //     trigger: '#home',
-    //     start: '50% 50%',
-    //     end: '+=300',
-    //     animation: animation,
-    //     onEnter: ({ progress, direction, isActive }) =>
-    //         console.log(progress, direction, isActive),
-    // })
-
+Splitting()
 const sticky = document.querySelector('.sticky')
-var epa = document.getElementById('epa')
-var leftEm = document.querySelector('.leftEm')
-    // var canvas = document.querySelector('#canvas')
+var loading = document.getElementById('loading')
 
-// var ctx = canvas.getContext('2d')
-// ctx.fillStyle = 'green'
-// ctx.fillRect(10, 10, 100, 100)
-window.addEventListener('load', (event) => {
-    gsap.fromTo(
-        '#epa', { x: 100 }, {
-            x: 100,
-            duration: 2000,
-        }
-    )
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    // multiplier: 1,
+    offset: ['30%', 0],
+    repeat: true,
+    tablet: { smooth: false },
+    smartphone: { smooth: false },
 })
-console.log('Loaded...')
 
-//Preload images and fonts
-// Promise.all([
-//     preloader('.menu__item'),
-//     preloadImages('.gallery__item-imginner'),
-//     preloadFonts('zkq2mjw'),
-// ]).then(() => {
-//     //Remove loader(loading class)
-//     document.body.classList.remove('loading')
+window.addEventListener('load', (event) => {
+    // gsap.registerPlugin(ScrollTrigger)
+    // scroll.on('scroll', ScrollTrigger.update)
+    // ScrollTrigger.scrollerProxy('.smooth-scroll', {
+    //     scrollTop(value) {
+    //         return arguments.length
+    //             ? locoScroll.scrollTo(value, 0, 0)
+    //             : locoScroll.scroll.instance.scroll.y
+    //     },
+    //     getBoundingClientRect() {
+    //         return {
+    //             top: 0,
+    //             left: 0,
+    //             width: window.innerWidth,
+    //             height: window.innerHeight,
+    //         }
+    //     },
+    // })
+    // var tl = gsap.timeline({ repeat: 2, repeatDelay: 1 })
+    // setTimeout(function () {
+    //     tl.to('.char', {
+    //         duration: 0.8,
+    //         stagger: 0.02,
+    //         opacity: 1,
+    //         y: 0,
+    //         ease: 'elastic',
+    //     })
+    // }, 4000)
+    scroll.on('scroll', (args) => {
+        if (typeof args.currentElements['epa'] === 'object') {
+            // let progress = args.currentElements['epa'].progress
+            console.dir(args.currentElements)
+            // var element = args.currentElements['epa']
 
-//     //Initialize custom cursor
-//     const cursor = new Cursor(document.querySelector('.cursor'))
-
-//     //initialize the MenuController
-//     // new MenuController(document.querySelector('.menu'));
-
-//     // Mouse effects on all links and buttons
-//     ;
-//     [...document.querySelectorAll('a, .gallery__item-more, .back')].forEach(
-//         (link) => {
-//             link.addEventListener('mouseenter', () => cursor.enter())
-//             link.addEventListener('mouseleave', () => cursor.leave())
-//         }
-//     )
-// })  )
-// })
+            // gsap.from(`#${element.section.el.id}.char`, {
+            //     duration: 0.8,
+            //     opacity: 0,
+            //     stagger: 0.02,
+            //     y: 20,
+            //     ease: 'elastic',
+            // })
+            for (const [key, value] of Object.entries(args.currentElements)) {
+                var element = value
+                // gsap.from(`#${element.section.el.id} * .char`, {
+                //     duration: 0.5,
+                //     opacity: 0,
+                //     stagger: 0.02,
+                //     y: 30,
+                //     ease: 'power1.inOut',
+                // })
+            }
+            gsap.from('.char', {
+                duration: 0.5,
+                opacity: 0,
+                stagger: 0.02,
+                y: 30,
+                ease: 'power1.inOut',
+            })
+            // args.currentElements.forEach((ele, index, array) => {})
+            // tl.progress(progress)
+        }
+    })
+})
